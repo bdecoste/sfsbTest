255edfbfbe1c6b4667cf453654e62ecea1f25f5e
Bill DeCoste <bdecoste@gmail.com>
2012-05-04 10:58:40 -0700

    jndi

--- src/org/jboss/sfsbTest/SfsbServlet.java
+++ src/org/jboss/sfsbTest/SfsbServlet.java
@@ -2,6 +2,7 @@ package org.jboss.sfsbTest;
 
 import java.io.IOException;
 import java.util.Properties;
+import java.util.StringTokenizer;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -55,53 +56,62 @@ public class SfsbServlet extends HttpServlet {
 	protected void callSfsb(HttpSession session) throws ServletException {
 		try {
 	    	
-			String remoting = System.getenv("OPENSHIFT_JBOSS_CLUSTER_REMOTING");
+			String remotings = System.getenv("OPENSHIFT_JBOSS_CLUSTER_REMOTING");
 			
-	    	Properties props = new Properties();
-	      
-	    	Properties jndiProps = new Properties();
-	    	jndiProps.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
-	    	jndiProps.put(InitialContext.PROVIDER_URL, "remote://" + remoting);
-	    	InitialContext jndiContext = new InitialContext(jndiProps);
-	    	System.out.println("!!! jndi props " + jndiContext.getEnvironment());
-	    	
-	    	String state = (String)session.getAttribute(STATE);
-	    	System.out.println("HTTPSession state " + state);
-	    	if (state == null){
-		    	session.setAttribute(STATE, MODIFIED);
-	    	}
-	    	
-	    	StatefulBean1Remote sfsb = (StatefulBean1Remote)session.getAttribute(SFSB);
-	    	System.out.println("HTTPSession sfsb " + sfsb);
-	    	
-	    	if (sfsb == null) {
-	    		String jndiBinding = "java:global/sfsbTest-1.0/StatefulBean1!org.jboss.jndiTest.StatefulBean1Remote";
-	    		sfsb = (StatefulBean1Remote) jndiContext.lookup(jndiBinding);
-	    		session.setAttribute(SFSB, sfsb);    		
-	    	}
-	    	
-	    	String jndiBinding = "java:global/sfsbTest-1.0/StatefulBean1!org.jboss.jndiTest.StatefulBean1Local";
-	    	StatefulBean1Local local = (StatefulBean1Local) jndiContext.lookup(jndiBinding);
-    	
-	    	System.out.println("State1 " + sfsb.getState());
-	    	sfsb.setState("MODIFIED");
-	    	System.out.println("State2 " + sfsb.getState());
-	    	
-	    	//jndiBinding = "java:global/sfsbTest/EntityTesterBean!org.jboss.jndiTest.EntityTester";
-	    	//EntityTester tester = (EntityTester)jndiContext.lookup(jndiBinding);
-	    	//tester.createEntity(1);
-	    	//tester.findEntity(1);
-	    	
-	    	JmsClient sm = new JmsClient(remoting);
-	        String msg = "Testing123";
-
-	        sm.sendMessageOverJMS(msg);
-	        
-	        System.out.println("JMS message sent");
-    	} catch (Exception e){
+			StringTokenizer tokenizer = new StringTokenizer(remotings, ",");
+			
+			while (tokenizer.hasMoreTokens()){
+				String remoting = tokenizer.nextToken();
+				runTest(session, remoting);
+			}
+		} catch (Exception e){
     		e.printStackTrace();
     		throw new ServletException(e);
     	}
+	}
+	
+	protected void runTest(HttpSession session, String remoting) throws Exception {
+    	Properties props = new Properties();
+      
+    	Properties jndiProps = new Properties();
+    	jndiProps.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+    	jndiProps.put(InitialContext.PROVIDER_URL, "remote://" + remoting);
+    	InitialContext jndiContext = new InitialContext(jndiProps);
+    	System.out.println("!!! jndi props " + jndiContext.getEnvironment());
+    	
+    	String state = (String)session.getAttribute(STATE);
+    	System.out.println("HTTPSession state " + state);
+    	if (state == null){
+	    	session.setAttribute(STATE, MODIFIED);
+    	}
+    	
+    	StatefulBean1Remote sfsb = (StatefulBean1Remote)session.getAttribute(SFSB);
+    	System.out.println("HTTPSession sfsb " + sfsb);
+    	
+    	if (sfsb == null) {
+    		String jndiBinding = "java:global/sfsbTest-1.0/StatefulBean1!org.jboss.jndiTest.StatefulBean1Remote";
+    		sfsb = (StatefulBean1Remote) jndiContext.lookup(jndiBinding);
+    		session.setAttribute(SFSB, sfsb);    		
+    	}
+    	
+    	String jndiBinding = "java:global/sfsbTest-1.0/StatefulBean1!org.jboss.jndiTest.StatefulBean1Local";
+    	StatefulBean1Local local = (StatefulBean1Local) jndiContext.lookup(jndiBinding);
+	
+    	System.out.println("State1 " + sfsb.getState());
+    	sfsb.setState("MODIFIED");
+    	System.out.println("State2 " + sfsb.getState());
+    	
+    	//jndiBinding = "java:global/sfsbTest/EntityTesterBean!org.jboss.jndiTest.EntityTester";
+    	//EntityTester tester = (EntityTester)jndiContext.lookup(jndiBinding);
+    	//tester.createEntity(1);
+    	//tester.findEntity(1);
+    	
+    	JmsClient sm = new JmsClient(remoting);
+        String msg = "Testing123";
+
+        sm.sendMessageOverJMS(msg);
+        
+        System.out.println("JMS message sent");
 	}
 	
 	public static void main(String args[]) throws Throwable
