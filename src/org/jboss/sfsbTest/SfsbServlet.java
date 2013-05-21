68af432e85e743b081bf7e589ddcd157df56c84e
William DeCoste <bdecoste@gmail.com>
2013-05-20 18:27:11 -0700

    update for amq

--- src/org/jboss/sfsbTest/SfsbServlet.java
+++ src/org/jboss/sfsbTest/SfsbServlet.java
@@ -1,11 +1,22 @@
 package org.jboss.sfsbTest;
 
 import java.io.IOException;
+
 import java.util.Properties;
 import java.util.StringTokenizer;
 
+import javax.jms.Connection;
+import javax.jms.DeliveryMode;
+import javax.jms.Destination;
+import javax.jms.ExceptionListener;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
 import javax.mail.Address;
-import javax.mail.Message;
+//import javax.mail.Message;
 import javax.mail.Provider;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
@@ -28,6 +39,7 @@ import org.jboss.jndiTest.StatefulBean1Remote;
 import org.jboss.jndiTest.StatelessBean1Remote;
 import org.jboss.jndiTest.TestEntity;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.log4j.*;
 
 /**
@@ -70,9 +82,8 @@ public class SfsbServlet extends HttpServlet {
 		try {
 			
 			LOG.info("########################### callSfsb ########################");
-	    	
-			String remotings = System.getenv("OPENSHIFT_JBOSSAS_CLUSTER_REMOTING");
-			System.out.println("remotings " + remotings);
+			
+			testActiveMQ();
 			
 	/*	    for (int i = 0 ; i < 10 ; ++i) {
 				StringTokenizer tokenizer = new StringTokenizer(remotings, ",");
@@ -83,8 +94,8 @@ public class SfsbServlet extends HttpServlet {
 				}
 			} */
 			
-			for (int i = 0 ; i < 20 ; ++i)
-				runTest(session, "localhost:5445");
+	//		for (int i = 0 ; i < 20 ; ++i)
+	//			runTest(session, "localhost:5445");
 			
 //			runTest(session, "be673bcb5b-bdecoste26e.dev.rhcloud.com[35545]");
 //			runTest(session, "scaledas7-bdecoste26e.dev.rhcloud.com[35540]");
@@ -94,6 +105,116 @@ public class SfsbServlet extends HttpServlet {
     		e.printStackTrace();
     		throw new ServletException(e);
     	}
+	}
+	
+	protected void testActiveMQ() throws Exception {
+		
+		String ip = System.getenv("OPENSHIFT_ACTIVEMQ_IP");
+		String port = System.getenv("OPENSHIFT_ACTIVEMQ_OPENWIRE_PORT");
+		String host = System.getenv("OPENSHIFT_ACTIVEMQ_OPENWIRE_HOST");
+		if (host != null && host.trim().length() > 0) {
+			ip = host;
+			port = System.getenv("OPENSHIFT_ACTIVEMQ_OPENWIRE_PORT");
+		}
+		consumeAmqMessage(ip, port);
+		sendAmqMessage(ip, port);
+	}
+		
+	protected void consumeAmqMessage(String ip, String port) throws Exception {
+        Thread consumerThread = new Thread(new AmqConsumer(ip, port));
+        consumerThread.setDaemon(false);
+        consumerThread.start();
+	}
+	
+	public class AmqConsumer implements Runnable, ExceptionListener {
+		
+		String ip;
+		String port;
+		
+		public AmqConsumer(String ip, String port) {
+			this.ip = ip;
+			this.port = port;
+		}
+        public void run() {
+            try {
+            	
+            	System.out.println("!!!!! consuming " + ip + " " + port);
+            	
+
+                // Create a ConnectionFactory
+                ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://" + ip + ":" + port);
+
+                // Create a Connection
+                Connection connection = connectionFactory.createConnection();
+                connection.start();
+
+                connection.setExceptionListener(this);
+
+                // Create a Session
+                Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+                // Create the destination (Topic or Queue)
+                Destination destination = session.createQueue("TEST.FOO");
+
+                // Create a MessageConsumer from the Session to the Topic or Queue
+                MessageConsumer consumer = session.createConsumer(destination);
+
+                // Wait for a message
+                Message message = consumer.receive(1000);
+
+                if (message instanceof TextMessage) {
+                    TextMessage textMessage = (TextMessage) message;
+                    String text = textMessage.getText();
+                    System.out.println("Received: " + text);
+                } else {
+                    System.out.println("Received: " + message);
+                }
+
+                consumer.close();
+                session.close();
+                connection.close();
+            } catch (Exception e) {
+                System.out.println("Caught: " + e);
+                e.printStackTrace();
+            }
+        }
+
+        public synchronized void onException(JMSException ex) {
+            System.out.println("JMS Exception occured.  Shutting down client.");
+        }
+    }
+	
+	protected void sendAmqMessage(String ip, String port) throws Exception {
+				
+		System.out.println("!!!!!! sending ip " + ip + " " + port);
+		
+		ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://" + ip + ":" + port);
+
+        // Create a Connection
+        Connection connection = connectionFactory.createConnection();
+        connection.start();
+
+        // Create a Session
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        // Create the destination (Topic or Queue)
+        Destination destination = session.createQueue("TEST.FOO");
+
+        // Create a MessageProducer from the Session to the Topic or Queue
+        MessageProducer producer = session.createProducer(destination);
+        producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+
+        // Create a messages
+        String text = "Hello world! From: " + Thread.currentThread().getName() + " : " + this.hashCode();
+        TextMessage message = session.createTextMessage(text);
+
+        // Tell the producer to send the message
+        System.out.println("Sent message: "+ message.hashCode() + " : " + Thread.currentThread().getName());
+        producer.send(message);
+
+        // Clean up
+        session.close();
+        connection.close();
 	}
 	
 	protected void viewJndi(Context ctx, String path) throws Exception {
@@ -118,7 +239,7 @@ public class SfsbServlet extends HttpServlet {
     	//viewJndi(jndiContext, "queue");
     	//viewJndi(jndiContext, "ejb");
     	
-   /* 	String state = (String)session.getAttribute(STATE);
+    	String state = (String)session.getAttribute(STATE);
     	System.out.println("HTTPSession state " + state);
     	if (state == null){
 	    	session.setAttribute(STATE, MODIFIED);
@@ -163,15 +284,15 @@ public class SfsbServlet extends HttpServlet {
     		entity = tester.createEntity(id, value);
     	} else {
     		System.out.println("found entity " + entity.getValue());
-    	}*/
+    	}
     	
-    	JmsClient sm = new JmsClient(remoting);
-        String msg = "Testing123";
+ //   	JmsClient sm = new JmsClient(remoting);
+ //       String msg = "Testing123";
 
-        sm.sendMessageOverJMS(msg);
+ //       sm.sendMessageOverJMS(msg);
 	}
 	
-	protected void testMail() throws Exception {
+/*	protected void testMail() throws Exception {
 		InitialContext ctx = new InitialContext();
 		javax.mail.Session session = (javax.mail.Session) ctx.lookup("java:jboss/mail/Default");
 		MimeMessage msg = new MimeMessage(session);
@@ -183,7 +304,7 @@ public class SfsbServlet extends HttpServlet {
 		trans.connect("smtp.gmail.com", "bdecoste", "TomBr@dy12");
 		trans.sendMessage(msg, msg.getAllRecipients());
 		trans.close();
-	}
+	}*/
 	
 	public static void main(String args[]) throws Throwable
     {        	    	
